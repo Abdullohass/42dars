9cc5974401483e1ad09264059fa14cff8a6d4064
Abdullohass <abdulloh.s.gk@gmail.com>
2024-09-07 15:21:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// let ism="Abdulloh";
+// var ism2="Abdulloh";
+
+// ism="Yusuf";
+
+// const ism3="Abdulloh";
+
+// console.log(ism)
+
+// alert("Salom")
+
+
+// let ism=prompt("Ismingizni kiriting")
+
+// console.log(ism)
+
+
+let ism = prompt("Ismingizni kiriting");
+let familya = prompt("Familyangizni kiriting");
+let yosh = prompt("Yoshingizni kiriting");
+let sinf = prompt("Oqiyotgan sinifingizni kiriting");
+let fan = prompt("Yoqtirgan faningizni kiriting");
+let tuman = prompt("Yashaydgon tumaningizni kiriting");
+let shahar = prompt("Tugulib osgan shahringizni kiriting");
+let lan = prompt("Nech hik til bikishingizni kiriting");
+let kasp = prompt("Kelajakdagi kaspingizni kiriting");
+let raqam = prompt("Tell raqamingizni kiriting");
+alert("Tashakur!!!")
+
+console.log("Sizning Ismingiz " + ism);
+console.log("Sizning Familyangiz " + familya);
+console.log("Sizning Yoshingiz " + yosh);
+console.log("Sizning Oqiyotgan sinifingiz " + sinf);
+console.log("Sizning Yoqtirgan faningiz " + fan);
+console.log("Sizning Yashaydgon tumaningiz " + tuman);
+console.log("Sizning Tugulib osgan shahringiz " + shahar);
+console.log("Sizning Nech hik til bikishingiz " + lan);
+console.log("Sizning Kelajakdagi kaspingiz " + kasp);
+console.log("Sizning Tell raqamingiz " + raqam);
+
+
+
+
+
+
